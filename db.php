6076b8f5d15e38ca5a525c8da8ea80869781e3fb
neroly <artempshik@yandex.ru>
2025-09-27 03:34:45 +0300

    port8889

--- db.php
+++ db.php
@@ -1,15 +1,15 @@
 <?php
-
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root"; 
 $dbname = "supermarket";
+$port = 8889; 
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 
 if (!$conn){
     die("Connection failed: " . mysqli_connect_error());
 } else {
-     "Успех";  
+    echo "Успех";  
 }
 ?>
